2e2c16f815b327f2f84a256f501c1d212cc12f0d
cryptolordperess <147560902+cryptolordperess@users.noreply.github.com>
2023-10-11 09:36:18 +0100

    Update OWNABLE.sol

--- OWNABLE.sol
+++ OWNABLE.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Context.sol";
+import "https://github.com/cryptolordperess/thepupclubtokenpresalecontracts/blob/main/CONTEXT.sol";
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
